924dfae1e323e4d6eba4a99a0d5b5a0151981fee
“AntonioAmbrosecchia” <antonyambro96@gmail.com>
2025-03-27 14:57:18 +0100

    add array of object and set app.jsx

---
+++ react-movie-filter/src/MovieFilter.jsx
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+import "./MovieFilter.css";
+
+const moviesList = [
+    { title: "Inception", genre: "Fantascienza" },
+    { title: "Il Padrino", genre: "Thriller" },
+    { title: "Titanic", genre: "Romantico" },
+    { title: "Batman", genre: "Azione" },
+    { title: "Interstellar", genre: "Fantascienza" },
+    { title: "Pulp Fiction", genre: "Thriller" },
+];
